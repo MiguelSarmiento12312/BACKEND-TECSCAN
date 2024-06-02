73f5b23ac36cee54fcfc41397c1653e12b3afc8b
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-06-02 16:42:02 -0500

    commit

---
+++ src/models/Medico.js
@@ -0,0 +1,14 @@
+// Medico.js
+
+class Medico {
+    constructor(nombre, apellido, email, password) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.email = email;
+      this.password = password;
+    }
+  
+  }
+  
+  export default Medico;
+  
