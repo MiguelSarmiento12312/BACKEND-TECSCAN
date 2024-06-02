c9342d5f0199dd35d436f74a473ea7b177e3b202
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-06-02 17:49:43 -0500

    c

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,30 +1,35 @@
 // loginController.js
 
 import bcrypt from 'bcrypt';
-import Medico from '../models/Medico.js';
+import { pool } from '../config/db.js'; // Importa el pool de conexión a MySQL si es necesario
 
 const loginController = {
   login: async (req, res) => {
     const { email, password } = req.body;
 
     try {
-      // Busca al médico por correo electrónico en la base de datos
-      const medico = await Medico.findOne({ where: { email } });
+      // Realiza la lógica de autenticación sin depender de un modelo específico
+      // Por ejemplo, puedes realizar una consulta directa a la tabla de medicos en tu base de datos MySQL
 
-      // Si el médico no existe, devolver un mensaje de error
-      if (!medico) {
+      // Realiza la consulta SQL para obtener los datos del médico por correo electrónico
+      const [rows] = await pool.query('SELECT * FROM medicos WHERE email = ?', [email]);
+
+      // Verifica si se encontró un médico con ese correo electrónico
+      if (rows.length === 0) {
         return res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
       }
+
+      const medico = rows[0];
 
       // Verifica si la contraseña coincide utilizando bcrypt
-      const passwordMatch = await bcrypt.compare(password, medico.password);
+      const isPasswordValid = await bcrypt.compare(password, medico.password);
 
-      // Si la contraseña no coincide, devolver un mensaje de error
-      if (!passwordMatch) {
+      // Si la contraseña no coincide, devuelve un mensaje de error
+      if (!isPasswordValid) {
         return res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
       }
 
-      // Si las credenciales son válidas, devolver un mensaje de éxito
+      // Si las credenciales son válidas, devuelve un mensaje de éxito
       return res.status(200).json({ success: true, message: 'Inicio de sesión exitoso' });
     } catch (error) {
       console.error(error);
