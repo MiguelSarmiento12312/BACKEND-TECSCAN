0a23192bb42cfce1ee76fd078abfd6ce90bfbd35
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-06-03 08:53:24 -0500

    new commit express-validator

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,3 @@
-// src/controllers/loginController.js
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { pool } from '../config/db.js';
@@ -8,26 +7,39 @@ const loginController = {
     const { email, password } = req.body;
 
     try {
+      // Buscar al médico en la base de datos por su correo electrónico
       const [rows] = await pool.query('SELECT * FROM medicos WHERE email = ?', [email]);
 
+      // Si no se encuentra ningún médico con el correo electrónico proporcionado, devolver un error de autenticación
       if (rows.length === 0) {
-        return res.status(401).json({ success: false, message: 'Email incorrecto' });
+        return res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
       }
 
+      // Obtener el primer médico encontrado (debería ser único debido a la restricción UNIQUE en la columna email)
       const medico = rows[0];
+
+      // Comparar la contraseña proporcionada con la contraseña almacenada en la base de datos
       const passwordMatch = await bcrypt.compare(password, medico.password);
 
+      // Depuración: Mostrar las contraseñas para verificar
+      console.log('Contraseña proporcionada:', password);
+      console.log('Contraseña almacenada:', medico.password);
+
+      // Si las contraseñas no coinciden, devolver un error de autenticación
       if (!passwordMatch) {
         return res.status(401).json({ success: false, message: 'Contraseña incorrecta' });
       }
 
+      // Generar un token de autenticación usando JWT
       const token = jwt.sign({ id: medico.id, email: medico.email }, process.env.JWT_SECRET, {
-        expiresIn: '1h'
+        expiresIn: '1h' // El token expira en 1 hora
       });
 
+      // Devolver el token de autenticación como respuesta
       return res.status(200).json({ success: true, token });
     } catch (error) {
       console.error('Error durante el inicio de sesión:', error);
+      // Si hay un error durante el proceso de inicio de sesión, devolver un error de servidor
       return res.status(500).json({ success: false, message: 'Error en el servidor' });
     }
   }
