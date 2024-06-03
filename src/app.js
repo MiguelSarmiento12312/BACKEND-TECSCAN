bbeb47fcc80148b810f4e817ca32cc4b0e34d838
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-06-03 09:07:18 -0500

    Commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-// src/app.js
 import express from 'express';
 import bodyParser from 'body-parser';
-import medicoRoutes from './routes/medicoRoutes.js';
+import medicoRoutes from './routes/medicoRoutes.js'; // Asegúrate de que la ruta es correcta
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Para parsear JSON en las peticiones
+
+// Usar las rutas de médicos
 app.use('/medicos', medicoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
