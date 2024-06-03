c107fcd5393ee710e5ade900f40ae531c12162ec
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-06-02 21:44:02 -0500

    mw

--- src/routes/medicoRoutes.js
+++ src/routes/medicoRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import authMiddleware from '../middleware/authMiddleware.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 import loginController from '../controllers/loginController.js';
 
 const router = express.Router();
