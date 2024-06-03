4744e227a065d166f33ace0424cae9e530d92438
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-06-02 22:23:53 -0500

    commit

--- src/routes/medicoRoutes.js
+++ src/routes/medicoRoutes.js
@@ -1,10 +1,14 @@
+// medicoRoutes.js
 import express from 'express';
 import medicoController from '../controllers/medicoController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-// Ruta para iniciar sesión de médico
-router.post('/login', authMiddleware, medicoController.login);
+// Ejemplo de una ruta POST con una función de callback
+router.post('/login', loginController.login);
+
+// Ejemplo de una ruta protegida que utiliza el middleware de autenticación
+router.get('/info', authMiddleware, medicoController.getMedicoInfo);
 
 export default router;
