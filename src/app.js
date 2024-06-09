b9baeee6d095b5d9adc111f1b6d95a9d03f781d8
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-06-09 12:35:03 -0500

    commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import medicoRoutes from './routes/medicoRoutes.js'; // Asegúrate de que la ruta es correcta
+import medicoRoutes from './routes/medicoRoutes.js';
+import pacientesRoutes from './routes/pacienteRoutes.js'; // Asegúrate de que la ruta es correcta
 import dotenv from 'dotenv';
 
 // Cargar variables de entorno desde el archivo .env
@@ -12,6 +13,9 @@ app.use(bodyParser.json()); // Para parsear JSON en las peticiones
 
 // Usar las rutas de médicos
 app.use('/medicos', medicoRoutes);
+
+// Usar las rutas de pacientes
+app.use('/pacientes', pacientesRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
